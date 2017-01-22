014749930de76f037642a8559e7e62ad422aedf7
Panu Poikselkä <panu.poikselka@palmu.fi>
2017-01-22 08:52:16 +0200

    Fixed buffer handling for node

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var mongoose = require('mongoose');
 var database = require('./app/config/database.js');
 var mood = require('./app/models/mood.js');
 var moodCtrl = require('./app/controllers/mood.js');
+var Buffer = require('buffer').Buffer;
 
 // Connnect to database
 mongoose.Promise = global.Promise;
